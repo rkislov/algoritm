4d2eb33dcc9a52e3f9435a0fa5bd2abb0969f89a
Roman Kislov <roman@kislovs.ru>
2019-04-19 14:48:03 +0500

    statistic model

---
+++ src/models/statistic.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const Task = mongoose.model('Task',{
+    counter_id: {
+        type: String,
+        trim: true,
+        required:true
+    },
+    gvs: {
+        type: Number,
+        default: 0
+    },
+    hvs: {
+        type: Number,
+        default: 0
+    },
+    el1: {
+        type: Number,
+        default: 0
+    },
+    el2: {
+        type: Number,
+        default: 0
+    },
+    el3: {
+        type: Number,
+        default: 0
+    },
+    gk: {
+        type: Number,
+        default: 0
+    },
+    volt: {
+        type: Number,
+        default: 0
+    },
+    amper: {
+        type: Number,
+        default: 0
+    },
+    tm: {
+        type: String,
+        default: 0
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+})
+
+module.exports = Task
